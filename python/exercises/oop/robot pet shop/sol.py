4bc8a940e0b2ca4f9d525fedcf9b3950ff53186e
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-02-26 13:22:21 +0200

    finalizing robot pet shop

--- python/exercises/oop/robot pet shop/sol.py
+++ python/exercises/oop/robot pet shop/sol.py
@@ -22,27 +22,55 @@ class Shop:
     def add_robot_pet(self, robot_pet):
         self.__robot_pets.append(robot_pet)
 
+    def __find_robot_pet(self, id = None, name = None):
+        if id == None and name == None:
+            raise Exception("Please provide and id or a name")
+        else:
+            for index, pet in enumerate(self.__robot_pets):
+                if pet.id == id or pet.name == name:
+                    return index, pet
+            # Couldn't find the pet in the shop:
+            raise Exception(f"Can't find robot with id {id} in the store")
+
     def get_balance(self):
         return self.__balance
 
     def sell(self, robot_pet_id):
-        for index, pet in enumerate(self.__robot_pets):
-            if pet.id == robot_pet_id:
-                print(f"Selling: {pet.name} id: {pet.id}")
-                wanted_pet = self.__robot_pets.pop(index)
-                wanted_pet.change_status("sold")
-                self.__balance += wanted_pet.price
-                return wanted_pet
-        
-        # Couldn't find the pet in the shop:
-        raise Exception(f"Can't find robot with i{robot_pet_id} in the store")
+        wanted_pet_index, wanted_pet_obj = self.__find_robot_pet(robot_pet_id)
+        wanted_pet_obj._change_status("sold")
+        self.__balance += wanted_pet_obj.price
+        self.__robot_pets.pop(wanted_pet_index)
+        print(f"Sold: {wanted_pet_obj.name} with price {wanted_pet_obj.price}")
+        return wanted_pet_obj
     
-    def get_all_available_pets_for_sale(self):
+    def get_all_with_status(self, status):
         for pet in self.__robot_pets:
-            if pet.status == "for sale":
+            if pet.status == status:
+                yield pet
+    
+    def get_for_sale_with_price_range(self, lower_price, higher_price):
+        for pet in self.get_all_with_status("for sale"):
+            if pet.price >= lower_price and pet.price <= higher_price:
                 yield pet
 
-
+    def get_employees_total_cost_per_day(self):
+        total = 0
+        for employee in self.__employees:
+            total += employee.daily_salary
+        
+        return total
+    
+    def get_robot_pet_details(self, id = None, name = None):
+        wanted_pet_index, wanted_pet_obj = self.__find_robot_pet(id, name)
+        details = ""
+        for attr in dir(wanted_pet_obj):
+            # Getting rid of dunder methods
+            if not attr.startswith("_"):
+                details += attr + ": " + str(getattr(wanted_pet_obj, attr)) + "\n"
+        return details
+                    
+            
+            
 class Robot:
     def __init__(self, name, id, battery_type):
         self.name = name
@@ -65,13 +93,13 @@ class Robot_pet(Robot):
         self.price = price
         self.cost_to_fix_per_day = cost_to_fix_per_day
         self.animal_type = animal_type
-        self.change_status(status)
+        self._change_status(status)
 
         if animal_type not in ["herbivore", "carnivore"]:
             raise Exception(f"Wrong animal type: {animal_type}")
 
 
-    def change_status(self, new_status):
+    def _change_status(self, new_status):
         if new_status not in ["for sale", "broken", "in repair", "to be shipped", "sold"]:
             raise Exception(f"Wrong status value: {new_status}")
         else:
@@ -93,13 +121,41 @@ our_shop.add_employee(emp2)
 pet1 = Robot_pet("pet1", 1, "lithium", "ss", 35, 5, "carnivore", "broken")
 pet2 = Robot_pet("pet2", 2, "alkaline", "plastic", 50, 2, "herbivore", "for sale")
 pet3 = Robot_pet("pet3", 3, "alkaline", "rubber", 60, 20, "carnivore", "for sale")
+pet4 = Robot_pet("pet4", 4, "lithium", "ss", 43, 10, "herbivore", "in repair")
+pet5 = Robot_pet("pet5", 5, "lithium", "ss", 15, 10, "herbivore", "for sale")
 
 our_shop.add_robot_pet(pet1)
 our_shop.add_robot_pet(pet2)
 our_shop.add_robot_pet(pet3)
+our_shop.add_robot_pet(pet4)
+our_shop.add_robot_pet(pet5)
 
+sold_pet = our_shop.sell(3)
 
-print("balance: ", our_shop.get_balance())
+# print all pets available for sale in the store
+print("\nAvailable pets for sale:")
+for pet in our_shop.get_all_with_status("for sale"):
+    print(pet.name)
 
-for item in our_shop.get_all_available_pets_for_sale():
-    print(item.name)
+# print all robots in repair
+print("\nRobots in repair:")
+for pet in our_shop.get_all_with_status("in repair"):
+    print(pet.name)
+
+#  print all pets available for sale, based on price range
+lower_price = 10
+higher_price = 50
+print("\nRobots for sale in defined price range:")
+for pet in our_shop.get_for_sale_with_price_range(lower_price, higher_price):
+    print(pet.name)
+
+# print all employees salary cost
+print(f"\nTotal daily salary cost: {our_shop.get_employees_total_cost_per_day()}")
+
+# print store balance
+print("\nTotal shop balance: ", our_shop.get_balance())
+
+# print robot details based on id or name
+pet_id_to_explore = 2
+print(f"\nDetails of robot pet with id = {pet_id_to_explore} are:" )
+print(our_shop.get_robot_pet_details(pet_id_to_explore))
