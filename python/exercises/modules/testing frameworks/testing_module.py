7fe0eebe93e011074e2833c4e13dbcef5d645073
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-03-03 11:54:58 +0200

    organized them into folders

--- python/exercises/modules/testing frameworks/testing_module.py
+++
@@ -1,5 +0,0 @@
-def is_equal(tested_val, num):
-    if tested_val == num:
-        print(f"Test success: {tested_val} == {num}")
-    else:
-        raise Exception(f"Test failed: {tested_val} == {num}")
