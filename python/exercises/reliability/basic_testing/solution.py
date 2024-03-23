29e48ebbd25e2a3b3d381cd48401a9caa73c8bac
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-03-24 00:47:58 +0200

    fixed solution.py

--- python/exercises/reliability/basic_testing/solution.py
+++ python/exercises/reliability/basic_testing/solution.py
@@ -1,15 +1,20 @@
 from Raffle import Raffle
 
-the_raffle = Raffle(3.4, 10)
-# the_raffle.sell_tickets("user0", 1)
-# the_raffle.sell_tickets("user1", 3)
-# the_raffle.sell_tickets("user2", 3)
-# the_raffle.sell_tickets("user3", 10)
-# the_raffle.sell_tickets("user4", 2)
+
+def main():
+    the_raffle = Raffle(3.4, 10)
+    the_raffle.sell_tickets("user0", 1)
+    the_raffle.sell_tickets("user1", 3)
+    the_raffle.sell_tickets("user2", 3)
+    the_raffle.sell_tickets("user3", 10)
+    the_raffle.sell_tickets("user4", 2)
+
+    winners = the_raffle.roll()
+
+    print("Winners of the Lottery are:")
+    for index, person in enumerate(winners):
+        print(f"{index+1}) {person}")
 
 
-winners = the_raffle.roll()
-
-print("Winners of the Lottery are:")
-for index, person in enumerate(winners):
-    print(f"{index+1}) {person}")
+if __name__ == "__main__":
+    main()
